a90ef762d06b7d0e14aeaab81a91cb23391c08a8
HaroldMth <167818233+HaroldMth@users.noreply.github.com>
2025-12-15 20:53:24 +0000

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ module.exports = {
   EMAIL: process.env.EMAIL || "tohidkhan9050482152@gmail.com",
   PASS: process.env.PASS || "Rvpy.B.6YeZn7CR",
   MESSAGE: process.env.MESSAGE || "Thank you for using our bot dont share your session id blah blah .etc",
-  IMAGE: process.env.IMAGE || "https://cdn.ironman.my.id/i/2iceb4.jpeg",
+  IMAGE: process.env.IMAGE || "https://i.ibb.co/9gCjCwp/OIG4-E-D0-QOU1r4-Ru-CKuf-Nj0o.jpg",
 }
