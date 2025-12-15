cf182175a78621eaf4ab8e9246c2e01e66575920
HaroldMth <167818233+HaroldMth@users.noreply.github.com>
2025-12-15 21:39:13 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+// USE ENVIRONMENT VARIABLES FOR EMAIL AND PASSWORD INSTEAD.
+module.exports = {
+  PREFIX: process.env.PREFIX || "THE-BYTE~",
+  EMAIL: process.env.EMAIL || "tohidkhan9050482152@gmail.com",
+  PASS: process.env.PASS || "Rvpy.B.6YeZn7CR",
+  MESSAGE: process.env.MESSAGE || "Thank you for using our bot dont share your session id blah blah .etc",
+  IMAGE: process.env.IMAGE || "https://cdn.ironman.my.id/i/2iceb4.jpeg",
+}
