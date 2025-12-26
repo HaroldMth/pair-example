41821a099d21587fc27b90f7ade967559fc31d1a
HaroldMth <mibeharold2@gmail.com>
2025-12-26 20:35:49 +0000

    FIXES

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ module.exports = {
   PREFIX: process.env.PREFIX || "THE-BYTE~",
   EMAIL: process.env.EMAIL || "tohidkhan9050482152@gmail.com",
   PASS: process.env.PASS || "Rvpy.B.6YeZn7CR",
-  MESSAGE: process.env.MESSAGE || "Thank you for using our bot dont share your session id blah blah .etc",
+  MESSAGE: process.env.MESSAGE || "✨ *HANS BYTE V2 PAIRED SUCCESSFULLY* ✨\n\nThank you for choosing *HANS BYTE* 🤖💜\nYour WhatsApp bot is now securely connected and ready to run.\n\n⚠️ *IMPORTANT SECURITY NOTICE*\n• Do NOT share your session ID or pairing code\n• Anyone with your session can fully control your bot\n• If leaked, regenerate a new session immediately\n\n📢 *Join our Official WhatsApp Channel*\nhttps://whatsapp.com/channel/0029Vb6F9V9FHWpsqWq1CF14\n\n🚀 *Open-source • Fast • Lightweight*\nGitHub Repository:\nhttps://github.com/HaroldMth/HANS_BYTE_v2/\n\n_Built with ❤️ by Hans Tech_",
   IMAGE: process.env.IMAGE || "https://i.ibb.co/9gCjCwp/OIG4-E-D0-QOU1r4-Ru-CKuf-Nj0o.jpg",
 }
